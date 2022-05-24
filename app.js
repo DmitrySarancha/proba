2aa881dadb484c1d9e2acc698c0feb064b001387
Dmitry Sarancha <lottyskound@gmail.com>
2022-05-24 17:15:45 +0300

    rename const and add load event for music

--- app.js
+++ app.js
@@ -1,78 +1,74 @@
-const drag = document.querySelector('.drag');
-const spin = document.querySelector('.spin');
-const img = spin.getElementsByTagName('img');
-const ground = document.querySelector('.ground');
-const aEle = [...img]; // ...aVid
-const radius = 240;
-const autoRotate = true;
-const rotateSpeed = -60;
-const imgWidth = 150;
-const imgHeight = 190;
+const drag = document.querySelector('.drag')
+const spin = document.querySelector('.spin')
+const img = spin.getElementsByTagName('img')
+const ground = document.querySelector('.ground')
+const aEle = [...img] // ...aVid
+const RADIUS = 240
+const AUTO_ROTATE = true
+const ROTATE_SPEED = -60
+const IMG_WIDTH = 150
+const IMG_HEIGHT = 190
 
 // size of img
-spin.style.width = imgWidth + 'px';
-spin.style.height = imgHeight + 'px';
+spin.style.width = IMG_WIDTH + 'px'
+spin.style.height = IMG_HEIGHT + 'px'
 
 // size of ground - depend on radius
-ground.style.width = radius * 3 + 'px';
-ground.style.height = radius * 3 + 'px';
+ground.style.width = RADIUS * 3 + 'px'
+ground.style.height = RADIUS * 3 + 'px'
 
 const init = (daleyTime) => {
-    for (let i = 0; i < aEle.length; i++) {
-        aEle[i].style.transform =
-            'rotateY(' +
-            i * (360 / aEle.length) +
-            'deg) translateZ(' +
-            radius +
-            'px)';
-        aEle[i].style.transition = 'transform 1s';
-        aEle[i].style.transitionDelay =
-            daleyTime || (aEle.length - i) / 3 + 's';
-    }
-};
+  for (let i = 0; i < aEle.length; i++) {
+    aEle[i].style.transform =
+      'rotateY(' + i * (360 / aEle.length) + 'deg) translateZ(' + RADIUS + 'px)'
+    aEle[i].style.transition = 'transform 1s'
+    aEle[i].style.transitionDelay = daleyTime || (aEle.length - i) / 3 + 's'
+  }
+}
 
 const applyTransform = (obj) => {
-    if (tY > 180) tY = 180;
-    if (tY < 0) tY = 0;
-    obj.style.transform = 'rotateX(' + tY + 'deg) rotateY(' + tX + 'deg)';
-};
+  if (tY > 180) tY = 180
+  if (tY < 0) tY = 0
+  obj.style.transform = 'rotateX(' + tY + 'deg) rotateY(' + tX + 'deg)'
+}
 
 let sX,
-    sY,
-    nX,
-    nY,
-    desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
+  sY,
+  nX,
+  nY,
+  desX = 0,
+  desY = 0,
+  tX = 0,
+  tY = 10
 
 const playSpin = (yes) => {
-    spin.style.animationPlayState = yes ? 'running' : 'paused';
-};
-setTimeout(init, 3000);
+  spin.style.animationPlayState = yes ? 'running' : 'paused'
+}
+setTimeout(init, 3000)
 
 // auto spin
 
-if (autoRotate) {
-    const animationName = rotateSpeed > 0 ? 'spin' : 'spinRevert';
-    spin.style.animation = `${animationName} ${Math.abs(
-        rotateSpeed
-    )}s infinite linear`;
+if (AUTO_ROTATE) {
+  const animationName = ROTATE_SPEED > 0 ? 'spin' : 'spinRevert'
+  spin.style.animation = `${animationName} ${Math.abs(
+    ROTATE_SPEED
+  )}s infinite linear`
 }
 
-// add backgraund music
+// add background music
 
-const bgMusicURL = '/adele-hello.mp3';
-const bgMusicControls = false;
+const bgMusicURL = '/adele-hello.mp3'
+const bgMusicControls = false
 
 if (bgMusicURL) {
-    document.getElementById(
-        'music-container'
-    ).innerHTML = `<audio src=${bgMusicURL} ${
-        bgMusicControls ? 'controls' : ' '
-    } autoplay loop>;
-
-	 <p> if you are reading this, it is bacause your browser does not support the audio element. </p>
-
-	 </audio>`;
+  const audio = `
+  <audio src=${bgMusicURL} ${bgMusicControls ? 'controls' : ' '} autoplay loop>;
+    <p> if you are reading this, it is because your browser does not support the audio element.</p>
+  </audio>`
+  document.getElementById('music-container').innerHTML = audio
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementsByTagName('audio')[0]
+  audio.play()
+})
